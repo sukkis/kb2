87db19515e0efeb14d2b87c93abd6c69ed30195a
Petteri Suckman <petteri.suckman@gmail.com>
2025-09-28 11:26:29 +0300

    added some fullstack app-running features to the run script

--- run.sh
+++ run.sh
@@ -10,6 +10,18 @@ test() { cd backend && deno task test --config=deno.json .; }
 # Run backend
 backend() { cd backend && deno task dev --config=deno.json .; }
 
+# Run backend with --watch
+backend_watch() { cd backend && deno task dev --watch --config=deno.json .; }
+
+# Run frontend
+frontend() { cd frontend && npm run dev; }
+
+# Run backend and frontend concurrently
+fullstack() { backend & frontend; wait; }
+
+# Run backend and frontend concurrently with backend in watch mode
+fullstack_watch() { backend_watch & frontend; wait; }
+
 # Format code
 fmt() { cd backend && deno fmt --check .; }
 
@@ -18,5 +30,9 @@ case "$1" in
   test) test ;;
   fmt) fmt ;;
   backend) backend ;;
-  *) echo "Usage: $0 {lint|fmt|test|backend}" ;;
+  backend_watch) backend_watch ;;
+  frontend) frontend ;;
+  fullstack) fullstack ;;
+  fullstack_watch) fullstack_watch ;;
+  *) echo "Usage: $0 {lint|fmt|test|backend|backend_watch|frontend|fullstack|fullstack_watch}" ;;
 esac
