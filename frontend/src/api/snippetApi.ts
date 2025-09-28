7896334766ae7f6a1c21cf3e150f17aa365c2f95
Petteri Suckman <petteri.suckman@gmail.com>
2025-09-28 16:59:21 +0300

    add delete handler to frontend for backend communication, cover all frontend handlers with tests, add RequestSnippet type for more safety

--- frontend/src/api/snippetApi.ts
+++ frontend/src/api/snippetApi.ts
@@ -1,36 +1,48 @@
-import { Snippet } from "../shared/types";
+import { Snippet, SnippetRequest } from "../shared/types";
 
+
+// fetch all snippets GET /snippets
 export async function fetchSnippets(): Promise<Snippet[]> {
   const res = await fetch("http://localhost:8087/snippets");
   if (!res.ok) throw new Error("Failed to fetch snippets");
   return await res.json();
 }
 
-// check /health from backend
+// check server responds, GET /health
 export async function checkHealth(): Promise<{ status: string }> {
   const res = await fetch("http://localhost:8087/health");
   if (!res.ok) throw new Error("Backend is not healthy");
   return await res.json();
 }
 
-// fetch a single snippet by id
+// fetch a single snippet by id, GET /snippets/:id
 export async function fetchSnippetById(id: string): Promise<Snippet> {
-  const res = await fetch(`http://localhost:8087/snippets/${id}`);
+  const res = await fetch(`http://localhost:8087/snippet/${id}`);
   if (!res.ok) throw new Error("Failed to fetch snippet");
   return await res.json();
 }
 
-// create a new snippet
+// create a new snippet, POST /add
 export async function createSnippet(
-  snippet: Omit<Snippet, "id" | "createdAt" | "updatedAt">,
+  title: string,
+  content: string
 ): Promise<Snippet> {
-  const res = await fetch("http://localhost:8087/snippets", {
+  const snippet: SnippetRequest = { title, content };
+  const res = await fetch(`http://localhost:8087/add`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(snippet),
   });
-  if (!res.ok) throw new Error("Failed to create snippet");
+  if (!res.ok) throw new Error(`Failed to create snippet: ${res.statusText}`);
   return await res.json();
+}
+
+// delete a snippet by id, DELETE /delete/:id
+export async function deleteSnippet(id: string): Promise<void> {
+  const res = await fetch(`http://localhost:8087/delete/${id}`, {
+    method: "DELETE",
+  });
+  if (!res.ok) throw new Error("Failed to delete snippet");
 }
